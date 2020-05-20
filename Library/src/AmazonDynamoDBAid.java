d885ec29ddaac314e0908dbf9d4f14451cbb5bdc
Duarte Miguel <duarte.miguel@tecnico.ulisboa.pt>
2020-05-20 13:06:52 +0100

    project base

---
+++ Library/src/AmazonDynamoDBAid.java
@@ -0,0 +1,131 @@
+package pt.ulisboa.tecnico.cnv.server;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import java.lang.InterruptedException;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.DescribeTableRequest;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
+import com.amazonaws.services.dynamodbv2.model.PutItemResult;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.dynamodbv2.model.TableDescription;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+
+public class AmazonDynamoDBAid{
+
+    private static AmazonDynamoDB amazonDynamoDB;
+
+    public static void init() throws Exception {
+
+    	ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+
+    	try {
+
+    		credentialsProvider.getCredentials();
+    	
+    	} catch (Exception e) {
+
+    		throw new AmazonClientException(
+    			"Cannot load the credentials from the credential profiles file. " +
+                "Please make sure that your credentials file is at the correct " +
+                "location (~/.aws/credentials), and is in valid format.",
+                e);
+    	
+    	}
+
+    	amazonDynamoDB = AmazonDynamoDBClientBuilder.standard().withCredentials(credentialsProvider).withRegion("us-east-1").build();
+    
+    	createTable("metrics");
+    }
+
+    public static void createTable(String tableName){
+
+    	try{
+
+    		CreateTableRequest createTableRequest = new CreateTableRequest().withTableName(tableName)
+            	.withKeySchema(new KeySchemaElement().withAttributeName("requestParameters").withKeyType(KeyType.HASH))
+            	.withAttributeDefinitions(new AttributeDefinition().withAttributeName("requestParameters").withAttributeType(ScalarAttributeType.S))
+            	.withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(1L).withWriteCapacityUnits(1L)); 
+    
+
+        	TableUtils.createTableIfNotExists(amazonDynamoDB, createTableRequest);
+        	TableUtils.waitUntilActive(amazonDynamoDB, tableName);
+
+        	DescribeTableRequest describeTableRequest = new DescribeTableRequest().withTableName(tableName);
+        	TableDescription tableDescription = amazonDynamoDB.describeTable(describeTableRequest).getTable();
+        	System.out.println("Table Description: " + tableDescription);	
+
+        } catch (AmazonServiceException ase) {
+        
+            System.out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to AWS, but was rejected with an error response for some reason.");
+            System.out.println("Error Message:    " + ase.getMessage());
+            System.out.println("HTTP Status Code: " + ase.getStatusCode());
+            System.out.println("AWS Error Code:   " + ase.getErrorCode());
+            System.out.println("Error Type:       " + ase.getErrorType());
+            System.out.println("Request ID:       " + ase.getRequestId());
+        
+        } catch (AmazonClientException ace) {
+        
+            System.out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with AWS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message: " + ace.getMessage());
+
+    	} catch (InterruptedException ie){
+			System.out.println("Interrupt exception");
+		}
+
+}
+
+    public static void addItem(Map<String, AttributeValue> newItem){
+    
+    	PutItemRequest putItemRequest = new PutItemRequest("metrics", newItem);
+    	PutItemResult putItemResult = amazonDynamoDB.putItem(putItemRequest);
+    	System.out.println("Result: " + putItemRequest);
+    }
+
+    public static List<Map<String, AttributeValue>> getItem(String requestParameters){
+    
+    	HashMap<String, Condition> scanFilter = new HashMap<String, Condition>();
+
+    	Condition condition = new Condition().withComparisonOperator(ComparisonOperator.EQ.toString())
+    		.withAttributeValueList(new AttributeValue(requestParameters));
+
+    	scanFilter.put("requestParameters", condition);
+    	ScanRequest scanRequest = new ScanRequest("metrics").withScanFilter(scanFilter);
+    	ScanResult scanResult = amazonDynamoDB.scan(scanRequest);
+
+    	return scanResult.getItems();
+    }
+
+    public static Map<String, AttributeValue> createItem(String requestParameters, long instructions, long basicBlocks){
+    
+    	Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
+    	item.put("requestParameters", new AttributeValue(requestParameters));
+    	item.put("instructions", new AttributeValue().withN(Long.toString(instructions)));
+    	item.put("basicBlocks",  new AttributeValue().withN(Long.toString(basicBlocks)));
+    
+    	return item;
+    }
+}
---
+++ Webserver/src/pt/ulisboa/tecnico/cnv/server/WebServer.java
@@ -0,0 +1,220 @@
+package pt.ulisboa.tecnico.cnv.server;
+
+import java.util.concurrent.atomic.AtomicLong;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.json.JSONArray;
+import pt.ulisboa.tecnico.cnv.server.AmazonDynamoDBAid;
+import pt.ulisboa.tecnico.cnv.solver.Solver;
+import pt.ulisboa.tecnico.cnv.solver.SolverArgumentParser;
+import pt.ulisboa.tecnico.cnv.solver.SolverFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Executors;
+
+public class WebServer {
+
+    private static Map<Long,ArrayList<AtomicLong>> metrics = new HashMap<>();
+    
+    public static Map<Long,ArrayList<AtomicLong>> getMap(){
+        return metrics;
+    }
+
+	public static void main(final String[] args) throws Exception {
+
+		//final HttpServer server = HttpServer.create(new InetSocketAddress("127.0.0.1", 8000), 0);
+
+		final HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+		server.createContext("/ping", new MyPingHandler());
+		server.createContext("/sudoku", new MyHandler());
+
+		// be aware! infinite pool of threads!
+		server.setExecutor(Executors.newCachedThreadPool());
+		server.start();
+
+		try {
+			FileWriter writer = new FileWriter("DeBug.txt", true);
+
+			writer.write("Phase 0\n");
+			writer.close();
+		} catch (IOException e){
+			System.out.println("Erro na escrita do ficheiro");
+		}
+
+		//System.out.println(server.getAddress().toString());
+	}
+
+	public static String parseRequestBody(InputStream is) throws IOException {
+        InputStreamReader isr =  new InputStreamReader(is,"utf-8");
+        BufferedReader br = new BufferedReader(isr);
+
+        // From now on, the right way of moving from bytes to utf-8 characters:
+
+        int b;
+        StringBuilder buf = new StringBuilder(512);
+        while ((b = br.read()) != -1) {
+            buf.append((char) b);
+
+        }
+
+        br.close();
+        isr.close();
+
+        return buf.toString();
+    }
+
+	static class MyPingHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange t) throws IOException {
+			FileWriter writer = new FileWriter("DeBug.txt", true);
+			writer.write("Phase Ping\n");
+			writer.close();
+
+			String response = "OK";
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+	}
+
+	static class MyHandler implements HttpHandler {
+		@Override
+		public void handle(final HttpExchange t) throws IOException {
+
+			ArrayList<AtomicLong> data = new ArrayList<AtomicLong>();
+			data.add(new AtomicLong(0));
+			data.add(new AtomicLong(0));
+			
+
+			metrics.put(Thread.currentThread().getId(), data);
+
+			// Get the query.
+			final String query = t.getRequestURI().getQuery();
+			//System.out.println("> Query:\t" + query);
+
+			// Break it down into String[].
+			final String[] params = query.split("&");
+
+			// Store as if it was a direct call to SolverMain.
+			final ArrayList<String> newArgs = new ArrayList<>();
+			for (final String p : params) {
+				final String[] splitParam = p.split("=");
+				newArgs.add("-" + splitParam[0]);
+				newArgs.add(splitParam[1]);
+			}
+			newArgs.add("-b");
+			newArgs.add(parseRequestBody(t.getRequestBody()));
+            
+			newArgs.add("-d");
+
+			// Store from ArrayList into regular String[].
+			final String[] args = new String[newArgs.size()];
+			int i = 0;
+			for(String arg: newArgs) {
+				args[i] = arg;
+				i++;
+			}	
+
+			// Get user-provided flags.
+			final SolverArgumentParser ap = new SolverArgumentParser(args);
+
+			// Create solver instance from factory.
+			final Solver s = SolverFactory.getInstance().makeSolver(ap);
+
+			try {
+				FileWriter writer = new FileWriter("DeBug.txt", true);
+
+				writer.write("Phase 3\n");
+				writer.close();
+			} catch (IOException e){
+				//System.out.println("Erro na escrita do ficheiro");
+			}
+
+			//Solve sudoku puzzle
+			JSONArray solution = s.solveSudoku();
+
+			// Send response to browser.
+			final Headers hdrs = t.getResponseHeaders();
+
+            //t.sendResponseHeaders(200, responseFile.length());
+            
+			///hdrs.add("Content-Type", "image/png");
+            hdrs.add("Content-Type", "application/json");
+
+			hdrs.add("Access-Control-Allow-Origin", "*");
+
+            hdrs.add("Access-Control-Allow-Credentials", "true");
+			hdrs.add("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+			hdrs.add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+            t.sendResponseHeaders(200, solution.toString().length());
+
+			try {
+				FileWriter writer = new FileWriter("DeBug.txt", true);
+
+				writer.write("Phase 4\n");
+				writer.close();
+			} catch (IOException e){
+				//System.out.println("Erro na escrita do ficheiro");
+			}
+
+            final OutputStream os = t.getResponseBody();
+            OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+            osw.write(solution.toString());
+            osw.flush();
+            osw.close();
+
+			os.close();
+				
+			//System.out.println("> Sent response to " + t.getRemoteAddress().toString());
+			ArrayList<AtomicLong> newData = metrics.get(Thread.currentThread().getId());
+			try{
+
+				AmazonDynamoDBAid.init();
+				AmazonDynamoDBAid.addItem(AmazonDynamoDBAid.createItem(ap.getN1() + "x" + ap.getN2() + " " + ap.getUn() + " " + ap.getSolverStrategy() , newData.get(0).get(), newData.get(1).get()));
+
+			}catch (IOException e){
+				e.printStackTrace();
+			}catch (Exception e){
+				e.printStackTrace();
+			}
+
+			try {
+				FileWriter writer = new FileWriter("DeBug.txt", true);
+
+				writer.write("Phase 5\n");
+				writer.close();
+			} catch (IOException e){
+				//System.out.println("Erro na escrita do ficheiro");
+			}
+
+		}
+	}
+
+	public void writeToFile(SolverArgumentParser ap, ArrayList<AtomicLong> newData){
+    	try {
+			FileWriter writer = new FileWriter("MetricsFile.txt", true);
+
+			writer.write("Tamanho da Sudoku: " + ap.getN1() + "x" + ap.getN2() + "\n");
+			writer.write("Posicoes por preencher: " + ap.getUn() + "\n");
+			writer.write("Estrategia Usada:" + ap.getSolverStrategy() + "\n");
+			writer.write("Instrucoes: " + newData.get(0).get() + " blocos: " + newData.get(1).get() + "\n\n");
+			writer.close();
+		} catch (IOException e){
+    		//System.out.println("Erro na escrita do ficheiro");
+		}
+	}
+
+}
